a71229e3075639163d24621cd19e739349d88c59
Ety <etienne.orio@orio.ch>
2026-04-26 13:32:03 +0200

    Fix many mistakes in logic to get adj and edges. Implement print_routers.

--- assignments/assignment-3/emulation.py
+++ assignments/assignment-3/emulation.py
@@ -11,11 +11,26 @@ def print_help():
         "  definition the definition file of the network in YAML\n\n"
         "options:\n"
         "  -h, --help show this help message and exit\n"
-        "  -d, --draw output a map of the routers in GraphViz format"
+        "  -d, --draw output a map of the routers in GraphViz format\n"
     )
 
 
-def parse():
+def print_routers(adj, edges):
+    router_nodes = [node for node in adj if node.startswith("r")]
+    router_edges = [edge for edge in edges if edge[0].startswith("r")]
+
+    print("graph Network {")
+
+    for node in router_nodes:
+        print(f"    {node} [shape=circle];")
+
+    for edge in router_edges:
+        print(f'    {edge[0]} -- {edge[1]} [label="{edge[2]}"];')
+
+    print("}")
+
+
+def parse_command_line():
     args = sys.argv
     args_count = len(args)
     min_args = 2
@@ -53,7 +68,7 @@ def parse():
 
 def calculate_subnet_address(ip, mask):
     ip_bytes = [int(x) for x in ip.split(".")]
-    mask_bytes = [int(x) for x in mask.split("x")]
+    mask_bytes = [int(x) for x in mask.split(".")]
 
     net_bytes = []
     for i in range(4):
@@ -65,18 +80,25 @@ def calculate_subnet_address(ip, mask):
 def get_subnets(topology):
     subnets = {}
 
-    for nodes in topology.values():
+    for category, nodes in topology.items():
         for node, interfaces in nodes.items():
             for config in interfaces.values():
                 subnet_id = calculate_subnet_address(config["address"], config["mask"])
                 if subnet_id not in subnets:
-                    subnets[subnet_id] = {"switch": None, "nodes": []}
+                    subnets[subnet_id] = {
+                        "routers": [],
+                        "hosts": [],
+                        "switch": None,
+                        "cost": 1,
+                    }
                 cost = config.get("cost", 1)
-                subnets[subnet_id]["nodes"].append((node, cost))
+                if cost > subnets[subnet_id]["cost"]:
+                    subnets[subnet_id]["cost"] = cost
+                subnets[subnet_id][category].append(node)
 
     switch_id = 1
     for subnet in subnets.values():
-        if len(subnet["nodes"]) > 2:
+        if len(subnet["hosts"]) >= 2:
             subnet["switch"] = f"s{switch_id}"
             switch_id += 1
 
@@ -87,20 +109,24 @@ def get_edges(subnets):
     edges = set()
 
     for subnet in subnets.values():
+        routers = subnet["routers"]
+        hosts = subnet["hosts"]
         switch = subnet["switch"]
-        nodes = subnet["nodes"]
-        if switch is not None:
-            for name, cost in nodes:
-                names = sorted([switch, name])
-                edge = (names[0], names[1], cost)
-                edges.add(edge)
-        else:
-            name_a, cost_a = nodes[0]
-            name_b, cost_b = nodes[1]
-            cost = max(cost_a, cost_b)
-            names = sorted([name_a, name_b])
-            edge = (names[0], names[1], cost)
+        cost = subnet["cost"]
+        if len(hosts) == 0:  # only routers
+            routers_n = len(routers)
+            for i in range(routers_n):
+                for j in range(i + 1, routers_n):
+                    edge = (routers[i], routers[j], cost)
+                    edges.add(edge)
+        elif len(hosts) == 1:  # router is guaranteed here
+            edge = (routers[0], hosts[0], cost)
             edges.add(edge)
+        else:  # hosts > 1 -> there is switch
+            all_nodes = routers + hosts
+            for node in all_nodes:
+                edge = (switch, node, cost)
+                edges.add(edge)
 
     return edges
 
@@ -119,16 +145,26 @@ def get_adjecency(edges):
     return adj
 
 
-def main():
-    result = parse()
+def parse_topology(topology):
+    subnets = get_subnets(topology)
+    edges = get_edges(subnets)
+    adj = get_adjecency(edges)
+    return adj, edges
 
+
+def main():
+    result = parse_command_line()
     if result is None:
         sys.exit(1)
-
     draw, topology_path = result
 
     with open(topology_path, "r") as file:
         topology = yaml.safe_load(file)
+    adj, edges = parse_topology(topology)
+
+    if draw:
+        print_routers(adj, edges)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
